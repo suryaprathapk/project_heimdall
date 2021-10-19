ab9d612bb9d02cf491280ffdf8ee9e2dabab1b94
skambham <skambham@modeln.com>
2021-10-20 00:09:01 +0530

    scrapper files update - changes to include google search

---
+++ eol_tracking/eol_tracking/spiders/centos_with_google_search.py
@@ -0,0 +1,42 @@
+import scrapy
+import json
+
+with open('C:\\suryas\\tech_stack_eol_tracking\google_crawler/centos.json') as f:
+    data = json.load(f)
+
+
+class centos_baseSpider1(scrapy.Spider):
+    name = "centos_base_with_search"
+    start_urls = [
+        data[0]['link']
+    ]
+
+    def parse(self, response):
+        filename = f'centos.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
+
+
+class centosSpider1(scrapy.Spider):
+    name = "centos_with_search"
+    start_urls = [
+        data[0]['link']
+    ]
+
+    # def parse(self, response):
+    #     for quote in response.css('table').getall():
+    #         yield {
+    #             'text': quote.css('span.text::text').get(),
+    #             'author': quote.css('small.author::text').get(),
+    #             'tags': quote.css('div.tags a.tag::text').getall(),
+    #         }
+    def parse(self, response):
+        for row in response.xpath('descendant-or-self::table/tbody/tr'):
+            yield {
+                'version': row.xpath('td[1]//text()').extract_first(),
+                'CentOS_6': row.xpath('td[2]//text()').extract_first(),
+                'CentOS_Linux_7': row.xpath('td[3]//text()').extract_first(),
+                'CentOS_Linux_8': row.xpath('td[4]//text()').extract_first(),
+                'CentOS_Stream_8': row.xpath('td[5]//text()').extract_first()
+            }
